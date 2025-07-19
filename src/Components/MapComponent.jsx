fb030964de82d12edbbf616884312f910e9d1945
Pranav <mpranavprem@gmail.com>
2025-07-19 10:04:50 +0530

    company address

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { companyDetails } from "../util/constant";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -12,7 +13,7 @@ L.Icon.Default.mergeOptions({
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
 });
 const MapComponent = () => {
-  const position = [12.888768, 77.597447];
+  const position = [19.096413, 78.342972];
 
   return (
     <div
@@ -29,23 +30,9 @@ const MapComponent = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={position}>
-          <Popup>
-            12.888768" 77.597447"
-            <br />
-            Salarpuria Symbiosis, Ground Floor, Bannerghatta Road, Arekere
-            Village, Begur Hobli, Bengaluru, Karnataka 560076
-          </Popup>
+          <Popup>{companyDetails.address}</Popup>
         </Marker>
       </MapContainer>
-      <div className="absolute top-2 left-2 bg-white text-black text-xs p-1 rounded">
-        49°51'22.0"N 2°22'33.5"E
-        <br />
-        Saint-Leu, France
-        <br />
-        <a href="#" className="text-blue-600">
-          View larger map
-        </a>
-      </div>
     </div>
   );
 };
